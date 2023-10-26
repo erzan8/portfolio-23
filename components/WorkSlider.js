a8c330378b836cdbbf0a83caec7f3f518df14141
GuilhemC <carroonn@hotmail.fr>
2023-10-26 18:57:21 +0200

    feat: template experiences

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -11,7 +11,9 @@ import {
 } from "react-icons/rx";
 import { IoIosBuild } from "react-icons/io";
 import { BsArrowRight } from "react-icons/bs";
-import { FreeMode, Pagination } from "swiper";
+import { Pagination } from "swiper";
+import Image from "next/image";
+import Link from "next/link";
 
 // data
 const workSlides = {
@@ -62,25 +64,44 @@ const workSlides = {
 const WorkSlider = () => {
   return (
     <Swiper
-      breakpoints={{
-        320: {
-          slidesPerView: 1,
-          spaceBetween: 15,
-        },
-        640: {
-          slidesPerView: 3,
-          spaceBetween: 15,
-        },
-      }}
+      spaceBetween={10}
       pagination={{
         clickable: true,
       }}
       freeMode={true}
       modules={[Pagination]}
-      className="h-[240px] sm:h-[380px]"
+      className="h-[280px] sm:h-[480px]"
     >
-      {workSlides.slides.map((item, index) => {
-        return <SwiperSlide key={index}></SwiperSlide>;
+      {workSlides.slides.map((slide, index) => {
+        return (
+          <SwiperSlide key={index}>
+            <div className="grid grid-cols-2 grid-rows-2 gap-4 ">
+              {slide.images.map((image, index) => {
+                return (
+                  <div
+                    key={index}
+                    className="relative rounded-lg overflow-hidden flex items-center justify-center group"
+                  >
+                    <div className="flex items-center justify-center relative overflow-hidden">
+                      <Image src={image.path} width={500} height={300} alt="" />
+                    </div>
+                    {/*Overlay*/}
+                    <div className="absolute inset-0 bg-gradient-to-l via-[#e838cc]  to-[#4a22bd] from-transparent opacity-0 group-hover:opacity-80 transition-all duration-500">
+                      <h3>Super projet</h3>
+                      <p>On adore</p>
+                      <Link href="" className="cursor-pointer">
+                        Lien github
+                      </Link>
+                      <Link href="" className="cursor-pointer">
+                        Lien site
+                      </Link>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </SwiperSlide>
+        );
       })}
     </Swiper>
   );
